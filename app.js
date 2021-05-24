d2ab152f59fcc9f9e87482f552a7bc22005ef7c2
unknown <georgecoll97@gmail.com>
2021-05-24 16:22:58 -0500

    html basics set

---
+++ app.js
@@ -0,0 +1,24 @@
+const months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
